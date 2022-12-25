aaf2927e7a740511a7c7faa0be6d326dba42594e
Misa <milosursulovic2696@gmail.com>
2022-12-25 16:41:20 +0100

    Add greeting section

--- app/src/main/java/com/example/meditationui/MainActivity.kt
+++ app/src/main/java/com/example/meditationui/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.meditationui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditationui.ui.HomeScreen
 import com.example.meditationui.ui.theme.MeditationUITheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +13,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationUITheme {
-
+                HomeScreen()
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MeditationUITheme {
+        HomeScreen()
     }
 }
